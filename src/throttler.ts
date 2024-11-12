dc6ef98a5060b2609bf6f6b1f2cf4d846c5e1d36
Daniel Gruszczyk <daniel.gruszczyk@live.com>
2024-11-12 10:38:02 +0000

    make throttler optional

--- src/throttler.ts
+++ src/throttler.ts
@@ -14,30 +14,27 @@ import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class GracefulKafkaThrottlerService {
-  private slidingWindowMs : number
-  private maxMessages : number
   private readonly logger = new Logger('GracefulKafkaThrottlerService')
 
   private timeoutsPerTopic: Map<string, { timestamp: number, timeout: NodeJS.Timeout }[]> = new Map()
 
-  constructor(configService: ConfigService) {
-    this.maxMessages = configService.get('kafkaThrottlerMaxMessages')
-    this.slidingWindowMs = configService.get('kafkaThrottlerSlidingWindowMs')
-  }
+  increment(topic: string, maxMessages: number, slidingWindowMs: number): number | null {
+    if (maxMessages === null || slidingWindowMs === null) {
+      return null
+    }
 
-  increment(topic: string): number | null {
     if (!this.timeoutsPerTopic.has(topic)) {
       this.timeoutsPerTopic.set(topic, [])
     }
     const timeouts = this.timeoutsPerTopic.get(topic)
     const context = {
-      slidingWindowMs: this.slidingWindowMs,
-      maxMessages: this.maxMessages,
+      slidingWindowMs: slidingWindowMs,
+      maxMessages: maxMessages,
       currentMessageCount: timeouts.length,
       topic
     }
 
-    if (timeouts.length >= this.maxMessages) {
+    if (timeouts.length >= maxMessages) {
       const unblockedInMs = Date.now() - timeouts[0].timestamp
       if (unblockedInMs > 0) {
         this.logger.warn('Throttling', { ...context, unblockedInMs })
@@ -51,7 +48,7 @@ export class GracefulKafkaThrottlerService {
       const [ oldest, ...rest ] = this.timeoutsPerTopic.get(topic)
       clearTimeout(oldest.timeout)
       this.timeoutsPerTopic.set(topic, rest)
-    }, this.slidingWindowMs)
+    }, slidingWindowMs)
     timeouts.push({ timestamp: Date.now(), timeout })
 
     return null
@@ -62,35 +59,45 @@ export class GracefulKafkaThrottlerService {
 export class GracefulKafkaThrottler implements NestInterceptor {
   private readonly logger = new Logger('GracefulKafkaThrottler')
 
+  private slidingWindowMs : number | null
+  private maxMessages : number | null
+
+  constructor(configService: ConfigService) {
+    this.maxMessages = configService.get('kafkaThrottlerMaxMessages', null)
+    this.slidingWindowMs = configService.get('kafkaThrottlerSlidingWindowMs', null)
+  }
+
   @Inject()
   private storage: GracefulKafkaThrottlerService
 
   async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
-    const ctx = context.switchToRpc().getContext() as KafkaContext
+    if (this.maxMessages && this.slidingWindowMs) {
+      const ctx = context.switchToRpc().getContext() as KafkaContext
 
-    const topic = ctx.getTopic();
-    const partition = ctx.getPartition();
-    const { offset } = ctx.getMessage();
-    const consumer = ctx.getConsumer()
+      const topic = ctx.getTopic();
+      const partition = ctx.getPartition();
+      const { offset } = ctx.getMessage();
+      const consumer = ctx.getConsumer()
 
-    const blockedMs = this.storage.increment(topic);
+      const blockedMs = this.storage.increment(topic, this.maxMessages, this.slidingWindowMs);
 
-    if (blockedMs) {
-      this.logger.warn(`Pause consumer for ${topic} for ${blockedMs}ms`)
-      consumer.seek({
-        topic,
-        partition,
-        offset: offset,
-      })
-      consumer.pause([{ topic, partitions: [partition] }])
-      setTimeout(async () => {
-        this.logger.log(`Unpausing consumer for ${topic}`)
-        const paused = consumer.paused()
-        if (paused.find((tp) => tp.topic === topic && tp.partitions.includes(partition))) {
-          consumer.resume([{ topic, partitions: [partition] }])
-        }
-      }, blockedMs)
-      return of(null)
+      if (blockedMs) {
+        this.logger.warn(`Pause consumer for ${topic}:${partition} for ${blockedMs}ms`)
+        consumer.seek({
+          topic,
+          partition,
+          offset: offset,
+        })
+        consumer.pause([{ topic, partitions: [partition] }])
+        setTimeout(async () => {
+          this.logger.log(`Unpause consumer for ${topic}:${partition}`)
+          const paused = consumer.paused()
+          if (paused.find((tp) => tp.topic === topic && tp.partitions.includes(partition))) {
+            consumer.resume([{ topic, partitions: [partition] }])
+          }
+        }, blockedMs)
+        return of(null)
+      }
     }
 
     return next.handle()
